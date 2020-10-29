bd37f225e92872809fe70b2f0047f215224ea4ae
Jachdich <jamesedkitching@gmail.com>
2020-10-29 17:08:54 +0000

    SQL database support

--- server.py
+++ server.py
@@ -1,25 +1,45 @@
-import os, threading, random, json, ssl, sys
+import os, threading, random, json, ssl, sys, mysql.connector, datetime
 from socket import *
 from struct import unpack, pack
 
-PATH = "/var/www/html"
+PATH = "/var/www/html/meme/"
+
+db = mysql.connector.connect(host="localhost", user="test", password="password", database="termimeme_metadata")
+dbc = db.cursor()
 
 class getmeta:
     def __init__(self, ID):
         self.ID = ID
         
     def __enter__(self):
-        print("getmeta for " + self.ID)
-        with open(PATH + "/meme/" + self.ID + ".metadata", "r") as f:
-            self.data = json.loads(f.read())
+        dbc.execute("select * from posts where id = {}".format(self.ID))
+        results = list(dbc)
+        if len(results) == 0: return None
+        if len(results) > 1 : raise RuntimeError(">= 2 posts with the same ID")
+        r = results[0]
+        self.data = {"id": r[0], "date": r[1], "title": r[2], "votes": r[3], "op": r[4]}
         return self.data
 
     def __exit__(self, type, value, traceback):
-        print("getmeta cleaning up for " + self.ID)
-        with open(PATH + "/meme/" + self.ID + ".metadata", "w") as f:
-            f.write(json.dumps(self.data))
+        d = self.data #I'm lazy ok
+        dbc.execute("update posts set date=%s, title=%s, votes=%s, op=%s where id=%s", (
+            d["date"].strftime("%Y-%m-%d %H:%M:%S"),
+            d["title"],
+            d["votes"],
+            d["op"],
+            d["id"]
+        ))
+        db.commit()
 
-
+def makemeta(d):
+    dbc.execute("insert into posts values (%s, %s, %s, %s, %s);", (
+            d["id"],
+            d["date"].strftime("%Y-%m-%d %H:%M:%S"),
+            d["title"],
+            d["votes"],
+            d["op"]
+        ))
+    db.commit()
 
 class ServerProtocol:
 
@@ -32,14 +52,11 @@ class ServerProtocol:
         (length,) = unpack('>Q', bs)
         data = b''
         while len(data) < length:
-            # doing it in batches is generally better than trying
-            # to do it all in one go, so I believe.
             to_read = length - len(data)
             data += self.connection.recv(
                 4096 if to_read > 4096 else to_read)
 
         if send_ack:
-            # send our 0 ack
             assert len(b'\00') == 1
             self.connection.sendall(b'\00')
         return data
@@ -58,12 +75,15 @@ class ServerProtocol:
 
     def index_posts(self):
         x = []
-        for f in os.listdir(PATH + "/meme/"):
+        for f in os.listdir(PATH):
             if f.endswith(".metadata"): continue
-            with open(PATH + "/meme/" + f + ".metadata", "r") as fp:
-                metadata = json.loads(fp.read())
-            with open(PATH + "/meme/" + f, "r") as fp:
+            
+            with getmeta(f) as meta:
+                metadata = meta.copy()
+            
+            with open(PATH + f, "r") as fp:
                 data = fp.read()
+            
             x.append({"data": data, **metadata})
         return x
             
@@ -72,6 +92,7 @@ class ServerProtocol:
         try:
             while True:
                 command = self.recv_msg(send_ack=False)
+                print(command)
                 if command == b"quit":
                     self.connection.sendall(b'\00')
                     break
@@ -90,31 +111,33 @@ class ServerProtocol:
                     continue                    
                 
                 if command == b"upload":
+                    self.connection.sendall(b'\00')
                     title = self.recv_msg().decode("utf-8")
                     data = self.recv_msg()
                     ID = self.gen_id()
-                    metadata = json.dumps({"title": title, "comments": [], "votes": 0, "op": self.user})
-                    with open(PATH + "/meme/" + ID, "wb") as f:
+                    with open(PATH + ID, "wb") as f:
                         f.write(data)
-                    with open(PATH + "/meme/" + ID + ".metadata", "w") as f:
-                        f.write(metadata)
+                    
+                    makemeta({"title": title, "date": datetime.datetime.now(), "votes": 0, "op": self.user, "id": ID})
                     self.connection.sendall(b'\00')
 
                 elif command == b"upvote":
+                    self.connection.sendall(b'\00')
                     ID = self.recv_msg().decode("utf-8")
                     with getmeta(ID) as meta:
-                        meta["votes"] += 1
-
-                    self.connection.sendall(b'\00')
-                    
+                        meta["votes"] += 1                    
                     
                 elif command == b"comment":
                     self.connection.sendall(b'\00')
-                    pass
+                    ID = self.recv_msg()
+                    body = self.recv_msg()
+                    
                 elif command == b"get":
+                    self.connection.sendall(b'\00')
                     sort = self.recv_msg()
                     x = self.index_posts()
-                    self.connection.sendall(b'\00')
+                    for n in x:
+                        n["date"] = n["date"].strftime("%Y-%m-%d %H:%M:%S")
                     self.send_msg(json.dumps(x).encode("utf-8"))
                 
         finally:
@@ -125,7 +148,7 @@ if __name__ == '__main__':
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     context.load_cert_chain(certfile="cert.pem", keyfile="cert.pem")
     s = socket(AF_INET, SOCK_STREAM)
-    s.bind(("127.0.0.1", 6969))
+    s.bind(("127.0.0.1", 6968))
     s.listen(100)
     ss = context.wrap_socket(s, server_side=True)
     clients = []
